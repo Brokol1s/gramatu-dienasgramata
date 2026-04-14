2212017d7a6882ea0450b5f4b35869ea89b061f9
Brokol1s <evsimcuks@gmail.com>
2026-04-14 15:43:13 +0300

    izdaritais darbins paslaik

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = 'papa_12'
+
+
+books = [{"id": 1, "nosaukums": "Grāmata A", "lapas": 250, "vertējums": 8},{"id": 2, "nosaukums": "Grāmata B", "lapas": 120, "vertējums": 9},]
+
+@app.route('/')
+def sakums():
+    return render_template('index.html')
+
+@app.route('/pieteikties', methods=['GET', 'POST'])
+def pieteikties():
+    if request.method == 'POST':
+        lietotajvards = request.form.get('username')
+        parole = request.form.get('password')
+        
+        if lietotajvards == 'admin':
+            return redirect(url_for('admin_panelis'))
+        return redirect(url_for('saraksts'))
+    return render_template('pieteikties.html')
+
+@app.route('/registreties', methods=['GET', 'POST'])
+def registreties():
+    if request.method == 'POST':
+        return redirect(url_for('pieteikties'))
+    return render_template('registreties.html')
+
+@app.route('/saraksts')
+def saraksts():
+    return render_template('saraksts.html', books=books)
+
+@app.route('/pievienot', methods=['GET', 'POST'])
+def pievienot():
+    if request.method == 'POST':
+        jauna_gramata = {
+            "id": len(books) + 1,
+            "nosaukums": request.form.get('nosaukums'),
+            "lapas": request.form.get('lapas'),
+            "vertējums": request.form.get('vertējums')
+        }
+        books.append(jauna_gramata)
+        return redirect(url_for('saraksts'))
+    return render_template('pievienot.html')
+
+@app.route('/admin')
+def admin_panelis():
+    return render_template('admin.html', books=books)
+
+@app.route('/dzest/<int:book_id>')
+def dzest_gramatu(book_id):
+    global books
+    books = [b for b in books if b['id'] != book_id]
+    return redirect(url_for('admin_panelis'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
